e36498e3d652b05ca48fd8f92f55323d14cc88f2
david.xavier <davidxaviersilva@gmail.com>
2022-05-09 15:05:58 -0300

    Fila

---
+++ src/main/java/one/digitalinovation/Main.java
@@ -0,0 +1,15 @@
+package one.digitalinovation;
+
+public class Main {
+    public static void main(String[]) args{
+
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue(new No("primeiro"));
+        minhaFila.enqueue(new No("segundo"));
+        minhaFila.enqueue(new No("terceiro"));
+        minhaFila.enqueue(new No("quarto"));
+
+        System.out.println(minhaFila);
+    }
+}
